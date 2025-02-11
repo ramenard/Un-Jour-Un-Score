8e747129f129fbbd11e9b025d6e8c2a14bc37639
Raphael Menard <raphael.menard@ynov.com>
2025-02-11 10:43:10 +0100

    init connection to db + user definition

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,37 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from './users/users.module';
+import {User} from "./users/entities/user.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('MYSQLHOST'),
+        port: +configService.get<number>('MYSQLPORT')!,
+        username: configService.get<string>('MYSQLUSER'),
+        password: configService.get<string>('MYSQL_ROOT_PASSWORD'),
+        database: configService.get<string>('MYSQL_DATABASE'),
+        entities: [User],
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useFactory: () => new ValidationPipe({ transform: true }),
+    },
+  ],
 })
 export class AppModule {}
---
+++ api/src/users/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+export enum RoleEnum {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column({ enum: RoleEnum, default: RoleEnum.USER })
+  role: RoleEnum;
+}
