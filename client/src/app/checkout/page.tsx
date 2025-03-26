3016f46031aeadcfa603cad0dea261aeaf13193b
Raphael Menard <raphael.menard@ynov.com>
2025-03-26 16:20:06 +0100

    chore: lint

--- client/src/app/checkout/page.tsx
+++ client/src/app/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useCallback, useEffect } from 'react';
+import React from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 
